5537e57ec5782b1957a26782e77f1ba0646186e9
ITB1LU <121174950+ITB1LU@users.noreply.github.com>
2022-12-21 17:21:31 +0000

    Login and Register Function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,78 @@
+import { useState } from 'react';
 import './App.css';
-
+import Form from './components/common/Form';
+import Home from './components/Home';
+import {
+  Routes,
+  Route,
+  useNavigate
+} from "react-router-dom";
+import { app } from './firebase-config';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import React, { useEffect } from 'react';
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  let navigate = useNavigate();
+
+  const handleAction = (id) => {
+    const authentication = getAuth();
+    if (id === 1) {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate('/home')
+          sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken)
+        })
+    }
+    else if (id === 2) {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate('/home')
+          sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken)
+        })
+    }
+  }
+  useEffect(() => {
+    let authToken = sessionStorage.getItem('Auth Token')
+
+    if (authToken) {
+      navigate('/home')
+    }
+  }, [])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <>
+        <Routes>
+          <Route
+            path='/login'
+            element={
+              <Form
+                title="Login"
+                setEmail={setEmail}
+                setPassword={setPassword}
+                handleAction={() => handleAction(1)}
+              />}
+          />
+          <Route
+            path='/register'
+            element={
+              <Form
+                title="Register"
+                setEmail={setEmail}
+                setPassword={setPassword}
+                handleAction={() => handleAction(2)}
+              />}
+          />
+
+          <Route
+            path='/home'
+            element={
+              <Home />}
+          />
+        </Routes>
+      </>
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC5kQ2uPVbNpM5ltLJ5_KhWn-XfOf7BRAI",
+    authDomain: "sapc-it1b1.firebaseapp.com",
+    projectId: "sapc-it1b1",
+    storageBucket: "sapc-it1b1.appspot.com",
+    messagingSenderId: "214180519223",
+    appId: "1:214180519223:web:f6520bc172918a925d0eb2",
+    measurementId: "G-S7QLQJ4PJL"
+  };
+
+export const app = initializeApp(firebaseConfig);
